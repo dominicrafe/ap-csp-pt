e14bfc2d92e63a75afa2b6370f03a43c1f8cc52d
Dominic Pacayra <156235742+dominicrafe@users.noreply.github.com>
2024-02-29 13:08:55 -0600

    finish anim | multiple enemy archetypes.

--- main.ts
+++ main.ts
@@ -68,6 +68,17 @@ sprites.onOverlap(SpriteKind.enemyProjectile, SpriteKind.Player, function (sprit
     info.changeLifeBy(-1)
     scene.cameraShake(4, 500)
 })
+function playerAtt (num: number) {
+    if (true) {
+        bullet = darts.create(assets.image`projectile`, SpriteKind.Projectile, player1.x, player1.y)
+        bullet.angle = 0 - spriteutils.radiansToDegrees(spriteutils.angleFrom(player1, xhair))
+        bullet.pow = speed3 * 4
+        bullet.gravity = 0
+        bullet.throwDart()
+        bullet.setFlag(SpriteFlag.DestroyOnWall, true)
+        projectileList.push(bullet)
+    }
+}
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite2, otherSprite2) {
     EnemyList.removeAt(EnemyList.indexOf(sprite2))
     info.changeLifeBy(-1)
@@ -85,19 +96,56 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite2, otherS
     sprites.destroy(explosion)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    bullet = darts.create(assets.image`projectile`, SpriteKind.Projectile, player1.x, player1.y)
-    bullet.angle = 0 - spriteutils.radiansToDegrees(spriteutils.angleFrom(player1, xhair))
-    bullet.pow = speed3 * 4
-    bullet.gravity = 0
-    bullet.throwDart()
-    bullet.setFlag(SpriteFlag.DestroyOnWall, true)
-    projectileList.push(bullet)
+    let gun = 0
+    playerAtt(gun)
 })
 function enemyMov (enemylist: Sprite[], speed: number) {
     for (let value of enemylist) {
         if (spriteutils.distanceBetween(value, player1) <= 100) {
-            value.follow(player1, speed)
-            enemyAtt(enemylist, speed)
+            if (value.image.equals(img`
+                . . f f f . . . . . . . . f f f 
+                . f f c c . . . . . . f c b b c 
+                f f c c . . . . . . f c b b c . 
+                f c f c . . . . . . f b c c c . 
+                f f f c c . c c . f c b b c c . 
+                f f c 3 c c 3 c c f b c b b c . 
+                f f b 3 b c 3 b c f b c c b c . 
+                . c b b b b b b c b b c c c . . 
+                . c 1 b b b 1 b b c c c c . . . 
+                c b b b b b b b b b c c . . . . 
+                c b c b b b c b b b b f . . . . 
+                f b 1 f f f 1 b b b b f c . . . 
+                f b b b b b b b b b b f c c . . 
+                . f b b b b b b b b c f . . . . 
+                . . f b b b b b b c f . . . . . 
+                . . . f f f f f f f . . . . . . 
+                `)) {
+                value.follow(player1, speed)
+                enemyAtt(enemylist, 0)
+            } else if (value.image.equals(img`
+                . . . . f f f f f . . . . . . . 
+                . . . f e e e e e f . . . . . . 
+                . . f d d d d e e e f . . . . . 
+                . c d f d d f d e e f f . . . . 
+                . c d f d d f d e e d d f . . . 
+                c d e e d d d d e e b d c . . . 
+                c d d d d c d d e e b d c . f f 
+                c c c c c d d d e e f c . f e f 
+                . f d d d d d e e f f . . f e f 
+                . . f f f f f e e e e f . f e f 
+                . . . . f e e e e e e e f f e f 
+                . . . f e f f e f e e e e f f . 
+                . . . f e f f e f e e e e f . . 
+                . . . f d b f d b f f e f . . . 
+                . . . f d d c d d b b d f . . . 
+                . . . . f f f f f f f f f . . . 
+                `)) {
+                value.follow(player1, speed / 2)
+                enemyAtt(enemylist, 500)
+            } else {
+                value.follow(player1, speed / 1.5)
+                enemyAtt(enemylist, 1000)
+            }
         }
     }
 }
@@ -160,47 +208,123 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
-controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, player1)
-    player1.setImage(img`
-        . . . f f f f f . . . . 
-        . . f e e e e e f f . . 
-        . f e e e e e e e f f . 
-        f e e e e e e e f f f f 
-        f e e 4 e e e f f f f f 
-        f e e 4 4 e e e f f f f 
-        f f e 4 4 4 4 4 f f f f 
-        f f e 4 4 f f 4 e 4 f f 
-        . f f d d d d 4 d 4 f . 
-        . . f b b d d 4 f f f . 
-        . . f e 4 4 4 e e f . . 
-        . . f 1 1 1 e d d 4 . . 
-        . . f 1 1 1 e d d e . . 
-        . . f 6 6 6 f e e f . . 
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    player1,
+    [img`
+        . . . . . . . . . . . . 
         . . . f f f f f f . . . 
-        . . . . . f f f . . . . 
-        `)
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        f f 4 d 4 d d d d f . . 
+        . f f f 4 d d b b f . . 
+        . 4 d d e 4 4 4 e f . . 
+        . e d d e 1 1 1 1 f . . 
+        . f e e f 6 6 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        . f 4 d 4 d d d d f . . 
+        . f f f e e d b b f . . 
+        . . f 4 d d e 4 e f . . 
+        . . f e d d e 1 1 f . . 
+        . f f f e e f 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . f f f f f . . . 
+        . . f f e e e e e f . . 
+        . f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f f 
+        . f 4 d 4 d d d d f f . 
+        . f f f 4 d d b b f . . 
+        . . f e e 4 4 4 e f . . 
+        . . 4 d d e 1 1 1 f . . 
+        . . e d d e 1 1 1 f . . 
+        . . f e e f 6 6 6 f . . 
+        . . . f f f f f f . . . 
+        . . . . f f f . . . . . 
+        `],
+    100,
+    true
+    )
 })
-controller.up.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, player1)
-    player1.setImage(img`
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    player1,
+    [img`
         . . . . f f f f . . . . 
         . . f f e e e e f f . . 
-        . f e e e e e e e f f . 
-        f f e f e e e e e e f f 
-        f f f e e e e e e e e f 
-        f f f e e e e e e f e f 
-        f f f f e e e e f f f f 
-        f f f f f f f f f f f f 
-        f f f f f f f f f f f f 
-        . f f f f f f f f f f . 
-        . e f f f f f f f f e . 
-        e 4 f b b b b b b f 4 e 
-        4 d f d d d d d d c d 4 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
         4 4 f 6 6 6 6 6 6 f 4 4 
         . . . f f f f f f . . . 
         . . . f f . . f f . . . 
-        `)
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e f f f . 
+        f f f e e e e e e f f f 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d 4 e f e 
+        f f f e 4 4 4 4 d d 4 e 
+        e 4 f b 1 1 1 e d d e . 
+        . . f 6 6 6 6 f e e . . 
+        . . f f f f f f f . . . 
+        . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e f f f . 
+        f f f e e e e e e f f f 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        e f e 4 d b b d d e f . 
+        e 4 d d 4 4 4 4 e f f f 
+        . e d d e 1 1 1 b f 4 e 
+        . . e e f 6 6 6 6 f . . 
+        . . . f f f f f f f . . 
+        . . . . . . . f f f . . 
+        `],
+    100,
+    true
+    )
 })
 info.onLifeZero(function () {
     game.setGameOverMessage(false, "u lost")
@@ -208,21 +332,23 @@ info.onLifeZero(function () {
 })
 function projMov (speed2: number, projList: Dart[]) {
     for (let value2 of projList) {
-        value2.pow = speed2 * 4
+        value2.pow = speed2 * 2
         value2.throwDart()
     }
 }
 function enemyAtt (list2: Sprite[], num: number) {
     for (let value3 of list2) {
-        if (randint(0, 500) == 1 && (speed3 != 0 && spriteutils.distanceBetween(value3, player1) <= 100)) {
-            enemyBullet = darts.create(assets.image`enemyProj`, SpriteKind.Projectile, value3.x, value3.y)
-            enemyBullet.angle = 0 - spriteutils.radiansToDegrees(spriteutils.angleFrom(value3, player1))
-            enemyBullet.pow = speed3 * 4
-            enemyBullet.gravity = 0
-            enemyBullet.throwDart()
-            enemyBullet.setKind(SpriteKind.enemyProjectile)
-            enemyBullet.setFlag(SpriteFlag.DestroyOnWall, true)
-            projectileList.push(enemyBullet)
+        if (num != 0) {
+            if (randint(0, num) == 1 && (speed3 != 0 && spriteutils.distanceBetween(value3, player1) <= 100)) {
+                enemyBullet = darts.create(assets.image`enemyProj`, SpriteKind.Projectile, value3.x, value3.y)
+                enemyBullet.angle = 0 - spriteutils.radiansToDegrees(spriteutils.angleFrom(value3, player1))
+                enemyBullet.pow = speed3 * 2
+                enemyBullet.gravity = 0
+                enemyBullet.throwDart()
+                enemyBullet.setKind(SpriteKind.enemyProjectile)
+                enemyBullet.setFlag(SpriteFlag.DestroyOnWall, true)
+                projectileList.push(enemyBullet)
+            }
         }
     }
 }
@@ -244,14 +370,15 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite3, ot
 let EnemySprite: Sprite = null
 let SpawnLocations: tiles.Location[] = []
 let enemyBullet: Dart = null
+let explosion: Sprite = null
 let projectileList: Dart[] = []
 let speed3 = 0
 let bullet: Dart = null
-let explosion: Sprite = null
 let EnemyList: Sprite[] = []
 let xhair: Sprite = null
 let player1: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
+scene.setBackgroundColor(15)
 player1 = sprites.create(assets.image`playerDef`, SpriteKind.Player)
 controller.moveSprite(player1, 100, 100)
 player1.setStayInScreen(false)
@@ -261,7 +388,7 @@ controller.player2.moveSprite(xhair, 175, 175)
 xhair.setStayInScreen(true)
 let CameraSprite = sprites.create(assets.image`clearSprite`, SpriteKind.camera)
 scene.cameraFollowSprite(CameraSprite)
-let EnemyImages = [img`
+let enemyImages = [img`
     . . f f f . . . . . . . . f f f 
     . f f c c . . . . . . f c b b c 
     f f c c . . . . . . f c b b c . 
@@ -314,12 +441,15 @@ let EnemyImages = [img`
     . . . . f f f f f f f f f . . . 
     `]
 EnemyList = []
+tiles.placeOnRandomTile(player1, sprites.dungeon.floorDark2)
+tiles.placeOnRandomTile(xhair, sprites.dungeon.floorDark2)
+xhair.setFlag(SpriteFlag.GhostThroughWalls, true)
 game.onUpdate(function () {
     CameraSprite.setPosition((player1.x + xhair.x) / 2, (player1.y + xhair.y) / 2)
     if (EnemyList.length == 0) {
         SpawnLocations = tiles.getTilesByType(sprites.dungeon.floorLight2)
         for (let index = 0; index < 10; index++) {
-            EnemySprite = sprites.create(EnemyImages._pickRandom(), SpriteKind.Enemy)
+            EnemySprite = sprites.create(enemyImages._pickRandom(), SpriteKind.Enemy)
             tiles.placeOnTile(EnemySprite, SpawnLocations.removeAt(randint(0, SpawnLocations.length - 1)))
             EnemyList.push(EnemySprite)
         }
@@ -328,8 +458,12 @@ game.onUpdate(function () {
 forever(function () {
     if (player1.vx == 0 && player1.vy == 0) {
         speed3 = 0
+        animation.stopAnimation(animation.AnimationTypes.All, player1)
+        if (xhair.vx != 0 || xhair.vy != 0) {
+            speed3 = 30
+        }
     } else {
-        speed3 = 50
+        speed3 = 100
     }
     enemyMov(EnemyList, speed3)
     projMov(speed3, projectileList)
